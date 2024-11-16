94b1301f3bb876f61764cbca05208db8f43e25ae
Jhon-Toro <toromurieljhon@gmail.com>
2024-11-16 16:14:59 -0500

    Adding develop branch

--- app/database.py
+++ app/database.py
@@ -2,7 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-DATABASE_URL = "postgresql://postgres:TWTegoxyjxiocuIZCskDKmTGzrlhJxCk@junction.proxy.rlwy.net:20665/railway"
+DATABASE_URL = "mysql+mysqlconnector://root@localhost:3306/integrador"
 
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
